ad50ce5d0a6ddf3e8266e4a82d2083efca651bff
Vishnu <vishnunroshan@bionworks.com>
2019-02-03 01:10:14 +0530

    initial commit
    setting up fonts and initial css theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
 
 @NgModule({
   declarations: [
@@ -14,7 +18,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    SocketIoModule.forRoot(config)
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +30,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// import * as figlet from 'figlet';
+import { Socket } from 'ng-socket-io';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+export class HomePage implements OnInit {
+  public roomName: string;
+  constructor(private socket: Socket, public navCtrl: NavController) {
   }
+
+  ngOnInit() {
+    // this.generateFiglet('Hello');
+  }
+
+  // generateFiglet(text): any {
+  //   figlet(text, function (err, figlet) {
+  //     console.log(figlet);
+  //   });
+  // }
+
+  joinChat() {
+    console.log(this.roomName)
+    this.socket.connect();
+    this.socket.emit('set-roomName', this.roomName);
+    // this.navCtrl.push('ChatRoomPage', { nickname: this.roomName });
+  }
+
 
 }
